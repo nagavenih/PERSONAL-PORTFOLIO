3069f762aa903e3662b6aa0d37719c7f534422aa
nagavenih <148582282+nagavenih@users.noreply.github.com>
2024-09-12 15:20:48 -0400

    weip

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ app.use(function(req, res, next) {
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server started on port ${PORT}!'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
