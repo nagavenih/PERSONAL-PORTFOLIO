556752f0bf547ccbd4808f1ec00694a5f6e55600
nagavenih <148582282+nagavenih@users.noreply.github.com>
2024-09-17 12:44:42 -0400

    index change

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #Welcome!
 ---
-This is my personal portfolio website designed to showcase my skills, projects, and experiences as a web developer. It includes sections such as About Me, Projects, Skills, and Contact.
+This is my personal portfolio website designed to showcase my skills, projects, and experiences as a web developer. It includes sections such as About Me, Projects, Skills, and Contact. 
--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require('path');
 
 app.use(express.json());
 
-app.use(express.static(__dirname + "/public"));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
-
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-Width,Content-Type,Accept,Authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     next();
 });
+
+
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
 
 const PORT = process.env.PORT || 3000;
